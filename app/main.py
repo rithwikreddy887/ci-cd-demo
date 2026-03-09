d65013dc94360f4e75c42758cc503eec9f410396
rithwik <rithwikreddy887@gmail.com>
2026-03-09 10:14:43 +0000

    Initial commit for CI/CD demo project

---
+++ app/main.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(add(2,3))
+    
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from app.main import add, multiply
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_multiply():
+    assert multiply(2,3) == 6
+    
